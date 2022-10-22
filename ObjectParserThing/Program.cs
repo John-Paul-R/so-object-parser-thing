afceadda9dd59dd4879fa0116bca2ede437276c0
John Paul Rutigliano <jp@jpcode.dev>
2022-10-22 15:33:55 -0400

    Simplify Program.cs

--- ObjectParserThing/Program.cs
+++ ObjectParserThing/Program.cs
@@ -1,18 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.Json;
 
-using System.Text.Json;
-using ObjectParserThing;
+Console.Write("Enter source file path: ");
+var line = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
-
-string StripComments(string line) { int idx = line.IndexOf('#'); return idx != -1 ? line[..idx] : line; }
-Console.WriteLine(StripComments("testing #123 gogo"));
-
-var outVal = new ObjectParserThing.ObjectParserThing().ParseFile(@"PATH_TO_FILE/SourceData.txt");
-
-Console.WriteLine(outVal);
+var outVal = new ObjectParserThing.ObjectParserThing().ParseFile(line);
 
 Console.WriteLine(JsonSerializer.Serialize(outVal, new JsonSerializerOptions {
     WriteIndented = true,
 }));
-Console.WriteLine("Done!");
